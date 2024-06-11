c031496856e894741d476dab66512d098ab920ef
antoniocristovam <antoniocristovam@outlook.com.br>
2024-06-11 19:23:10 -0300

    feat: add react-navigation/native

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,18 @@
+import { Home } from "@screens/Home";
+import { Profile } from "@screens/Profile";
+import { History } from "@screens/History";
+import { Exercise } from "@screens/Exercise";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="home" component={Home} />
+      <Screen name="history" component={History} />
+      <Screen name="profile" component={Profile} />
+      <Screen name="exercise" component={Exercise} />
+    </Navigator>
+  );
+}
