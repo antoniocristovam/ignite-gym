943ff39b21bb58b807273ecba8e0e6ed6333d289
antoniocristovam <antoniocristovam@outlook.com.br>
2024-06-14 19:23:00 -0300

    feat: finish screen Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,17 @@
 import { UserPhoto } from "@components/UserPhoto";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { Center, ScrollView, VStack, Skeleton, Text } from "native-base";
+import {
+  Center,
+  ScrollView,
+  VStack,
+  Skeleton,
+  Text,
+  Heading,
+} from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 import { Input } from "@components/input";
+import { Button } from "@components/button";
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
@@ -43,6 +51,19 @@ export function Profile() {
           <Input placeholder="Nome" bg={"gray.600"} />
           <Input isDisabled placeholder="E-mail" bg={"gray.600"} />
         </Center>
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color={"gray.200"} fontSize={"md"} mb={2}>
+            Alterar senha
+          </Heading>
+          <Input placeholder="Senha antiga" bg={"gray.600"} secureTextEntry />
+          <Input placeholder="Nova senha" bg={"gray.600"} secureTextEntry />
+          <Input
+            placeholder="Confirme nova senha"
+            bg={"gray.600"}
+            secureTextEntry
+          />
+          <Button title="Atualizar" mt={4} />
+        </VStack>
       </ScrollView>
     </VStack>
   );
