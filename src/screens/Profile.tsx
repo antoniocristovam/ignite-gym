fc8bfad37751e74e163c9201f83e52ff2e496f90
antoniocristovam <antoniocristovam@outlook.com.br>
2024-06-14 18:59:44 -0300

    feat: add user profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,20 @@
-import { Center, Text } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, VStack } from "native-base";
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color={"white"}>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{ uri: "https://github.com/antoniocristovam.png" }}
+            alt="Foto de perfil de Antonio Cristovam"
+            size={33}
+          />
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
