5ea13620395d0e7e0d5e9764c20e3bb4bc416b58
antoniocristovam <antoniocristovam@outlook.com.br>
2024-06-14 18:52:33 -0300

    feat: add ListEmptyComponent

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,6 +1,6 @@
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { Heading, SectionList, VStack } from "native-base";
+import { Heading, SectionList, Text, VStack } from "native-base";
 import { useState } from "react";
 
 export function History() {
@@ -41,6 +41,17 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={
+          exercicios.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <>
+            <Text color={"gray.100"} textAlign={"center"}>
+              Não há exercicios registrados ainda. {"\n"} Vamos fazer exercícios
+              hoje ?
+            </Text>
+          </>
+        )}
       />
     </VStack>
   );
