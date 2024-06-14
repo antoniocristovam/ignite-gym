5969b99f7fe6e2f93786be225beeffc065cf609d
antoniocristovam <antoniocristovam@outlook.com.br>
2024-06-14 19:10:38 -0300

    feat: add skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,31 @@
-import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, ScrollView, VStack } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Center, ScrollView, VStack, Skeleton } from "native-base";
+import { useState } from "react";
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const PHOTO_SIZE = 33;
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: "https://github.com/antoniocristovam.png" }}
-            alt="Foto de perfil de Antonio Cristovam"
-            size={33}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded={"full"}
+              startColor={"gray.500"}
+              endColor={"gray.300"}
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/antoniocristovam.png" }}
+              alt="Foto de perfil de Antonio Cristovam"
+              size={PHOTO_SIZE}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
