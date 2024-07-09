395bc93a1541f3935142048c0c7b6c30b2868561
antoniocristovam <antoniocristovam@outlook.com.br>
2024-07-09 18:38:32 -0300

    feat: add api.post

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,8 +1,11 @@
 import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
 import { createContext, useState } from "react";
+import { Alert } from "react-native";
 
 export type AuthContextDataProps = {
   user: UserDTO;
+  signIn: (mail: string, password: string) => Promise<void>;
 };
 
 export const AuthContext = createContext<AuthContextDataProps>(
@@ -14,14 +17,25 @@ type AuthContextProviderProps = {
 };
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState({
-    id: "a",
-    name: "John asas",
-    email: "tste@gmail.com",
-    avatar: "an.png",
-  });
+  const [user, setUser] = useState({} as UserDTO);
+
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post("/sessions", { password, email });
+
+      if (data.user) {
+        setUser(data.user);
+      }
+    } catch (error: string | any) {
+      Alert.alert(error.message);
+
+      throw error;
+    }
+  }
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, signIn }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
