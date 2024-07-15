7367bf3a300eeb585ecf4f1e3905ac776d1aeace
antoniocristovam <antoniocristovam@outlook.com.br>
2024-07-15 19:59:17 -0300

    feat: Refactor HistoryCard component to accept data prop

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,6 +1,13 @@
+import { HistoryDTO } from "@dtos/HistoryDTO";
 import { HStack, Heading, Text, VStack } from "native-base";
 
-export function HistoryCard() {
+type Props = {
+  data: HistoryDTO;
+};
+
+export function HistoryCard({ data }: Props) {
+  console.log(data);
+
   return (
     <HStack
       w="full"
@@ -20,14 +27,14 @@ export function HistoryCard() {
           fontFamily={"heading"}
           numberOfLines={1}
         >
-          Costas
+          {data.group}
         </Heading>
         <Text color={"gray.100"} fontSize={"lg"} numberOfLines={1}>
-          Puxada Frontal
+          {data.name}
         </Text>
       </VStack>
       <Text color={"gray.300"} fontSize={"md"}>
-        10:00
+        {data.hour}
       </Text>
     </HStack>
   );
