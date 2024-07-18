72e31945fc467dbae55a77c8094c7cd8994adbc7
antoniocristovam <antoniocristovam@outlook.com.br>
2024-07-18 18:28:10 -0300

    feat: add function to get photo api

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -8,6 +8,7 @@ import { UserPhoto } from "./UserPhoto";
 import { useAuth } from "@hooks/useAuth";
 
 import defaultUserImage from "@assets/userPhotoDefault.png";
+import { api } from "@services/api";
 
 export function HomeHeader() {
   const { user, signOut } = useAuth();
@@ -15,7 +16,11 @@ export function HomeHeader() {
   return (
     <HStack bg={"gray.600"} pt={16} pb={5} px={8} alignItems={"center"}>
       <UserPhoto
-        source={user.avatar ? { uri: user.avatar } : defaultUserImage}
+        source={
+          user.avatar
+            ? { uri: `${api.defaults.baseURL}/avatar/${user.avatar}` }
+            : defaultUserImage
+        }
         alt="Foto de perfil de Antonio Cristovam"
         size={16}
         mr={4}
